a9e78f934a76dbf9d00fa54266958efa518b8ba4
Deepak-rock <deepakind777@gmail.com>
2024-12-14 12:48:46 +0530

    test commit

--- src/components/PrimeVideo/index.js
+++ src/components/PrimeVideo/index.js
@@ -1,1 +1,29 @@
 // Write your code here
+import MoviesSlider from '../MoviesSlider'
+import './index.css'
+
+const PrimeVideo = props => {
+  const {moviesList} = props
+  const categoryComdey = moviesList.filter(
+    eachItem => eachItem.categoryId === 'COMEDY',
+  )
+  const categoryAction = moviesList.filter(
+    eachItem => eachItem.categoryId === 'ACTION',
+  )
+  return (
+    <div className="prime-video-app-container">
+      <img
+        className="banner-img"
+        src="https://assets.ccbp.in/frontend/react-js/prime-video-img.png"
+        alt="prime video"
+      />
+      <div className="movies-container">
+        <h1 className="movie-category">Action Movies</h1>
+        <MoviesSlider moviesList={categoryAction} />
+        <h1 className="movie-category">Comdey Movies</h1>
+        <MoviesSlider moviesList={categoryComdey} />
+      </div>
+    </div>
+  )
+}
+export default PrimeVideo
